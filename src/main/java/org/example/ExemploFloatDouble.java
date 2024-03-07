ab2123f7c4d543e999d2e1484d600ee318427193
samue <samuel.jdev11@gmail.com>
2024-03-07 10:54:06 -0400

    Tipos primitivos - float e double

---
+++ src/main/java/org/example/ExemploFloatDouble.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class ExemploFloatDouble {
+    public static void main(String[] args) {
+        double peso = 84.9d; // "d" é opcional
+        System.out.println(peso);
+
+        // float taxa = 1_294.93; // não compila
+        float taxa = 1_294.93f;
+        System.out.println(taxa);
+    }
+
+}
