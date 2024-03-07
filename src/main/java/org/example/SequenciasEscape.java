e521d005d0a58a61524f851726e646640a60fe3d
samue <samuel.jdev11@gmail.com>
2024-03-07 16:12:13 -0400

    Usando sequências de escape

---
+++ src/main/java/org/example/SequenciasEscape.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class SequenciasEscape {
+    public static void main(String[] args) {
+        System.out.println("Oi \"Maria\"");
+
+        System.out.println("Seu nome:\nJoão");
+
+        System.out.println("C:\\Windows");
+    }
+}
