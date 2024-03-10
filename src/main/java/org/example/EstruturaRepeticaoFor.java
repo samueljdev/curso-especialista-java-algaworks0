ed96542c30713fd187cbe0eac62d6fc479ec914a
samue <samuel.jdev11@gmail.com>
2024-03-09 21:26:08 -0400

    Estrutura de repetição for

---
+++ src/main/java/org/example/EstruturaRepeticaoFor.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class EstruturaRepeticaoFor {
+    public static void main(String[] args) {
+        // for (int i = 1; i <= 10; i++) {
+        //   System.out.println(i);
+        // }
+
+        // for (int i = 10; i > 0; i--) {
+        //   System.out.println(i);
+        // }
+
+        for (; ; ) {
+            System.out.println("Teste");
+        }
+
+    }
+}
